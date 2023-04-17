c5613e1ad1a783bdad6a3556c383860e37168059
gm9450 <goldmanbo213@gmail.com>
2023-04-17 16:47:04 +0900

    실습1

---
+++ 파이썬 실습/chapter09_실습1.py
@@ -0,0 +1,28 @@
+from turtle import *
+import random
+
+colorList = ['red', 'blue', 'orange', 'purple', 'brown', 'pink', 'green']
+
+
+class MyTurtle(Turtle):
+    def run(self, x, y):
+        self.fillcolor(random.choice(colorList))
+        self.right(random.randrange(0, 360))
+        total_move = random.randrange(10, 100)
+        a = 0
+        b = 0
+        for i in range(total_move):
+            if (-400 < a < 400 and -400 < b < 400):
+                self.fd(1)
+                (a, b) = self.position()
+            else:
+                break
+        if (-400 >= a or a >= 400 or -400 >= b or b >= 400):
+            self.right(180)
+            self.fd(20)
+
+
+t = MyTurtle()
+t.shape("turtle")
+t.width(3)
+t.onclick(t.run)
