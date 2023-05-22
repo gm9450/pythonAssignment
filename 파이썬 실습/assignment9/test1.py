9ba8335d8ba0a4371a45ffe71a41af3dd847fbb3
gm9450 <goldmanbo213@gmail.com>
2023-05-22 20:17:38 +0900

    ad

--- 파이썬 실습/assignment9/test1.py
+++ 파이썬 실습/assignment9/test1.py
@@ -1,89 +1,284 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import filedialog
+import os
+
+#추가한 기능은 없습니다
 
 def encrypt_text():
     # 파일 대화 상자 열기
-    file_path = filedialog.askopenfilename(filetypes=[('텍스트 파일', '*.txt')])
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
 
     if file_path:
-        try:
+        # 키워드 입력을 위한 윈도우 창 생성
+        keyword_window = tk.Tk()
+        keyword_window.title("암호화 키워드 입력")
+
+        # 키워드 입력을 위한 레이블 및 텍스트 상자 생성
+        keyword_label = tk.Label(keyword_window, text="암호화에 사용할 키워드를 입력하세요:")
+        keyword_label.pack()
+
+        keyword_entry = tk.Entry(keyword_window)
+        keyword_entry.pack()
+
+        def confirm_keyword():
+            # 키워드 확인 및 윈도우 창 닫기
+            keyword = keyword_entry.get()
+            keyword_window.destroy()
+
             # 파일 읽기
-            with open(file_path, 'r') as file:
-                text = file.read()
+            with open(file_path, "r", encoding="utf-8") as file:
+                original_text = file.read()
 
             # 텍스트 암호화
-            encrypted_text = vigenere_cipher(text, keyword='SECRET')  # 암호화 함수 호출 (비즈네르 암호를 예시로 사용)
+            encrypted_text = vigenere_cipher(original_text, keyword)
 
-            # 새로운 파일 생성하여 암호화된 텍스트 저장
-            save_path = filedialog.asksaveasfilename(defaultextension='.tenc', filetypes=[('암호화된 파일', '*.tenc')])
-            with open(save_path, 'w') as file:
+            # 저장 파일 경로 생성
+            save_path = get_encrypted_filename1(file_path)
+
+            # 암호화된 텍스트 파일 저장
+            with open(save_path, "w", encoding="utf-8") as file:
                 file.write(encrypted_text)
 
-            messagebox.showinfo("암호화 완료", "텍스트 암호화가 완료되었습니다.")
-        except IOError:
-            messagebox.showerror("에러", "파일을 읽는 중에 오류가 발생했습니다.")
-    else:
-        messagebox.showwarning("경고", "파일을 선택하지 않았습니다.")
+        # 확인 버튼 생성
+        confirm_button = tk.Button(keyword_window, text="확인", command=confirm_keyword)
+        confirm_button.pack()
+
+        keyword_window.mainloop()
 
 def decrypt_text():
     # 파일 대화 상자 열기
-    file_path = filedialog.askopenfilename(filetypes=[('암호화된 파일', '*.tenc')])
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.tenc")])
 
     if file_path:
-        try:
+        # 키워드 입력을 위한 윈도우 창 생성
+        keyword_window = tk.Tk()
+        keyword_window.title("복호화 키워드 입력")
+
+        # 키워드 입력을 위한 레이블 및 텍스트 상자 생성
+        keyword_label = tk.Label(keyword_window, text="복호화에 사용할 키워드를 입력하세요:")
+        keyword_label.pack()
+
+        keyword_entry = tk.Entry(keyword_window)
+        keyword_entry.pack()
+
+        def confirm_keyword():
+            # 키워드 확인 및 윈도우 창 닫기
+            keyword = keyword_entry.get()
+            keyword_window.destroy()
+
             # 파일 읽기
-            with open(file_path, 'r') as file:
+            with open(file_path, "r", encoding="utf-8") as file:
                 encrypted_text = file.read()
 
             # 텍스트 복호화
-            decrypted_text = vigenere_decipher(encrypted_text, keyword='SECRET')  # 복호화 함수 호출 (비즈네르 암호의 복호화 과정)
+            decrypted_text = vigenere_decipher(encrypted_text, keyword)
 
-            # 새로운 파일 생성하여 복호화된 텍스트 저장
-            save_path = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=[('텍스트 파일', '*.txt')])
-            with open(save_path, 'w') as file:
+            # 저장 파일 경로 생성
+            save_path = get_decrypted_filename1(file_path)
+
+            # 복호화된 텍스트 파일 저장
+            with open(save_path, "w", encoding="utf-8") as file:
                 file.write(decrypted_text)
 
-            messagebox.showinfo("복호화 완료", "텍스트 복호화가 완료되었습니다.")
-        except IOError:
-            messagebox.showerror("에러", "파일을 읽는 중에 오류가 발생했습니다.")
-    else:
-        messagebox.showwarning("경고", "파일을 선택하지 않았습니다.")
+        # 확인 버튼 생성
+        confirm_button = tk.Button(keyword_window, text="확인", command=confirm_keyword)
+        confirm_button.pack()
+
+        keyword_window.mainloop()
+
+def encrypt_file():
+    # 파일 대화 상자 열기
+    file_path = filedialog.askopenfilename()
+
+    if file_path:
+        # 키워드 입력을 위한 윈도우 창 생성
+        keyword_window = tk.Tk()
+        keyword_window.title("암호화 키워드 입력")
+
+        # 키워드 입력을 위한 레이블 및 텍스트 상자 생성
+        keyword_label = tk.Label(keyword_window, text="암호화에 사용할 키워드를 입력하세요:")
+        keyword_label.pack()
+
+        keyword_entry = tk.Entry(keyword_window)
+        keyword_entry.pack()
+
+        def confirm_keyword():
+            # 키워드 확인 및 윈도우 창 닫기
+            keyword = keyword_entry.get()
+            keyword_window.destroy()
+
+            # 이진 파일 읽기
+            with open(file_path, "rb") as file:
+                original_data = file.read()
+
+            # 이진 파일 암호화
+            encrypted_data = rc4_cipher(original_data, keyword)
+
+            # 저장 파일 경로 생성
+            save_path = get_encrypted_filename2(file_path)
+
+            # 암호화된 이진 파일 저장
+            with open(save_path, "wb") as file:
+                file.write(encrypted_data)
+
+        # 확인 버튼 생성
+        confirm_button = tk.Button(keyword_window, text="확인", command=confirm_keyword)
+        confirm_button.pack()
+
+        keyword_window.mainloop()
+
+def decrypt_file():
+    # 파일 대화 상자 열기
+    file_path = filedialog.askopenfilename(filetypes=[("Binary Files", "*.benc")])
+
+    if file_path:
+        # 키워드 입력을 위한 윈도우 창 생성
+        keyword_window = tk.Tk()
+        keyword_window.title("복호화 키워드 입력")
+
+        # 키워드 입력을 위한 레이블 및 텍스트 상자 생성
+        keyword_label = tk.Label(keyword_window, text="복호화에 사용할 키워드를 입력하세요:")
+        keyword_label.pack()
+
+        keyword_entry = tk.Entry(keyword_window)
+        keyword_entry.pack()
+
+        def confirm_keyword():
+            # 키워드 확인 및 윈도우 창 닫기
+            keyword = keyword_entry.get()
+            keyword_window.destroy()
+
+            # 이진 파일 읽기
+            with open(file_path, "rb") as file:
+                encrypted_data = file.read()
+
+            # 이진 파일 복호화
+            decrypted_data = rc4_decipher(encrypted_data, keyword)
+
+            # 저장 파일 경로 생성
+            save_path = get_decrypted_filename2(file_path)
+
+            # 복호화된 이진 파일 저장
+            with open(save_path, "wb") as file:
+                file.write(decrypted_data)
+
+        # 확인 버튼 생성
+        confirm_button = tk.Button(keyword_window, text="확인", command=confirm_keyword)
+        confirm_button.pack()
+
+        keyword_window.mainloop()
+
+def get_encrypted_filename1(file_path):
+    # 파일명과 확장자 분리
+    file_name, file_ext = os.path.splitext(file_path)
+
+    # 암호화된 파일명 생성
+    encrypted_file_name = file_name + ".tenc"
+
+    return encrypted_file_name
+
+def get_encrypted_filename2(file_path):
+    # 파일명과 확장자 분리
+    file_name, file_ext = os.path.splitext(file_path)
+
+    # 암호화된 파일명 생성
+    encrypted_file_name = file_name + ".benc"
+
+    return encrypted_file_name
+
+def get_decrypted_filename1(file_path):
+    # 파일명과 확장자 분리
+    file_name, file_ext = os.path.splitext(file_path)
+
+    # 복호화된 파일명 생성
+    decrypted_file_name = file_name + "_복호화" + ".txt"
+
+    return decrypted_file_name
+
+
+def get_decrypted_filename2(file_path):
+    # 파일명과 확장자 분리
+    file_name, file_ext = os.path.splitext(file_path)
+
+    # 복호화된 파일명 생성
+    decrypted_file_name = file_name + "_복호화"
+
+    return decrypted_file_name
 
 def vigenere_cipher(text, keyword):
-    # 비즈네르 암호 알고리즘 구현 (예시)
-    # 여기에서 실제 암호화 알고리즘을 구현하시면 됩니다.
-    # 이 예시에서는 각 문자를 키워드의 문자와 XOR 연산하여 암호화합니다.
-    encrypted_text = ''
+    # 비즈네르 암호화 수행
+    encrypted_text = ""
     keyword_length = len(keyword)
     for i, char in enumerate(text):
-        key_char = keyword[i % keyword_length]
-        encrypted_char = chr(ord(char) ^ ord(key_char))
+        key = keyword[i % keyword_length]
+        encrypted_char = chr((ord(char) + ord(key)) % 256)
         encrypted_text += encrypted_char
     return encrypted_text
 
 def vigenere_decipher(text, keyword):
-    # 비즈네르 암호 복호화 알고리즘 구현 (예시)
-    # 여기에서 실제 복호화 알고리즘을 구현하시면 됩니다.
-    # 이 예시에서는 각 문자를 키워드의 문자와 XOR 연산하여 복호화합니다.
-    decrypted_text = ''
+    # 비즈네르 복호화 수행
+    decrypted_text = ""
     keyword_length = len(keyword)
     for i, char in enumerate(text):
-        key_char = keyword[i % keyword_length]
-        decrypted_char = chr(ord(char) ^ ord(key_char))
+        key = keyword[i % keyword_length]
+        decrypted_char = chr((ord(char) - ord(key)) % 256)
         decrypted_text += decrypted_char
     return decrypted_text
 
-window = tk.Tk()
-window.title("메뉴 예제")
+def rc4_cipher(data, key):
+    # RC4 암호화 수행
+    S = list(range(256))
+    j = 0
+    encrypted_data = bytearray()
 
-menu = tk.Menu(window)
+    # 키를 바이트로 변환
+    key = key.encode('utf-8')
 
-encryption_menu = tk.Menu(menu, tearoff=0)
-encryption_menu.add_command(label="텍스트 암호화", command=encrypt_text)
-encryption_menu.add_command(label="텍스트 복호화", command=decrypt_text)
+    # 초기화 단계
+    for i in range(256):
+        j = (j + S[i] + key[i % len(key)]) % 256
+        S[i], S[j] = S[j], S[i]
 
-menu.add_cascade(label="암호화 메뉴", menu=encryption_menu)
+    # 암호화 단계
+    i = 0
+    j = 0
+    for byte in data:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        k = S[(S[i] + S[j]) % 256]
+        encrypted_data.append(byte ^ k)
 
-window.config(menu=menu)
+    return encrypted_data
 
-window.mainloop()
+
+
+def rc4_decipher(data, key):
+    # RC4 복호화 수행
+    return rc4_cipher(data, key)
+
+
+def main():
+    root = tk.Tk()
+    root.title("암호화 GUI")
+    root.geometry("400x300")
+
+    menubar = tk.Menu(root)
+
+    encrypt_menu = tk.Menu(menubar, tearoff=0)
+    encrypt_menu.add_command(label="텍스트 암호화", command=encrypt_text)
+    encrypt_menu.add_command(label="파일 암호화", command=encrypt_file)
+
+    decrypt_menu = tk.Menu(menubar, tearoff=0)
+    decrypt_menu.add_command(label="텍스트 복호화", command=decrypt_text)
+    decrypt_menu.add_command(label="파일 복호화", command=decrypt_file)
+
+    menubar.add_cascade(label="암호화", menu=encrypt_menu)
+    menubar.add_cascade(label="복호화", menu=decrypt_menu)
+
+    root.config(menu=menubar)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
